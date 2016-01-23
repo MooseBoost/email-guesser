c8a45d68b5a8d4ff1fc1049a6c0f9f990a7884bc
Nathaniel Miller <dukeoflaser@gmail.com>
2016-01-23 17:18:35 +0000

    add Predictor class to predict email addresses based on name, company, and other  email addresses

--- lib/predictor.rb
+++ lib/predictor.rb
@@ -1,3 +1,41 @@
+require 'open-uri'
 require 'json'
 
-# your code here
+class Predictor
+  
+  def initialize(json_file)
+    @dataset = JSON.parse(File.read(json_file))
+  end
+  
+  
+  def guess(full_name, company_name)
+
+    @company = company_name
+    @name = full_name.downcase.split
+  
+    template = @dataset.values.detect do |address|
+      address.include? @company
+    end
+    
+    if template == nil
+      ex_adds = ["a.b@c", "a.bb@c", "aa.b@c", "aa.bb@c"]
+    else
+      ex_adds = [template]
+      # ex_adds << template
+    end
+    
+    email_guess = ex_adds.collect do |address|
+      user = address.slice(0...(address.index('@')))
+      components = user.split('.')
+      
+      components[0].length == 1 ? first = @name.first[0] : first = @name.first
+      components[1].length == 1 ? last = @name.last[0] : last = @name.last
+      
+      "#{first}.#{last}@#{@company}"
+    end
+      
+    email_guess
+  end
+end
+
+	
